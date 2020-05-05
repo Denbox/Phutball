cfbdf211700b9ec9ca3dc84b0a01f17c30b17f85
Jacob Denbeaux <denbeaux@mit.edu>
2020-05-04 23:01:49 -0400

    accrued technical debt with ugly functions, but added opacity to jumped people

--- src/Phutball.elm
+++ src/Phutball.elm
@@ -20,7 +20,8 @@ type alias Coords =
 
 type alias Ball =
   { coords : Coords
-  , history : List Coords
+  , landed : List Coords
+  , jumped : List Coords
   }
 
 type alias MouseData =
@@ -68,7 +69,7 @@ inDraggableBounds : Coords -> Bool
 inDraggableBounds coords = (List.member coords.x (List.range 0 14)) && (List.member coords.y (List.range 0 20))
 
 validJump : Coords -> Game -> Bool
-validJump coords game = allowedLine game.ball.coords coords && allPeopleInBetween coords game
+validJump coords game = allowedLine game.ball.coords coords && peopleInBetween coords game
 
 diag : Coords -> Coords -> Bool
 diag start end = (abs (start.x - end.x)) == (abs (start.y - end.y))
@@ -79,30 +80,33 @@ cross start end = (start.x == end.x) || (start.y == end.y)
 allowedLine : Coords -> Coords -> Bool
 allowedLine start end = (diag start end) || (cross start end)
 
-peopleInBetween : Coords -> Game -> List Coords
-peopleInBetween coords game =
+jumpedSquares : Coords -> Coords -> List Coords
+jumpedSquares p1 p2 =
   let
-    p1 = coords
-    p2 = game.ball.coords
     dx = if p1.x == p2.x then 0 else (abs (p2.x-p1.x)) // (p2.x-p1.x)
     dy = if p1.y == p2.y then 0 else (abs (p2.y-p1.y)) // (p2.y-p1.y)
     n = Basics.max (abs (p2.x-p1.x)) (abs (p2.y-p1.y))
   in
     List.map (\i -> Coords (p1.x+dx*i) (p1.y+dy*i)) (List.range 1 (n-1))
 
-allPeopleInBetween : Coords -> Game -> Bool
-allPeopleInBetween coords game =
+peopleInBetween : Coords -> Game -> Bool
+peopleInBetween coords game =
   if Basics.max (abs (coords.x - game.ball.coords.x)) (abs (coords.y - game.ball.coords.y)) == 1 then
     False
   else
-    List.all (\x -> List.member x game.people) (peopleInBetween coords game)
+    List.all (\x -> List.member x game.people) (jumpedSquares coords game.ball.coords)
 
-removeJumped : Coords -> Game -> List Coords
-removeJumped coords game =
-  List.filter (\x -> not <| List.member x (peopleInBetween coords game)) game.people
+isDoubleJump : Coords -> Game -> Bool
+-- isDoubleJump start game = List.all (\x -> List.member x (jumpedHistorySquares game)) (jumpedSquares start game.ball.coords)
+isDoubleJump start game = List.any (\x -> List.member x game.ball.jumped) (jumpedSquares start game.ball.coords)
+
+-- should just be ball
+removeJumped : Game -> List Coords
+removeJumped game =
+  List.filter (\x -> not <| List.member x game.ball.jumped) game.people
 
 validDrag : Coords -> Game -> Bool
-validDrag coords game = (empty coords game) && (inDraggableBounds coords) && (validJump coords game)
+validDrag coords game = (empty coords game) && (inDraggableBounds coords) && (validJump coords game) && (not (isDoubleJump coords game))
 
 
 main =
@@ -113,7 +117,7 @@ main =
     , view = view
   }
 
-game_init = Game (Ball (Coords 7 9) []) [] X
+game_init = Game (Ball (Coords 7 9) [] []) [] X
 mouse_init = MouseData False (Coords -1 -1) (Coords -1 -1)
 
 init : () -> (Model, Cmd Msg)
@@ -137,9 +141,12 @@ update msg model =
             -- try to place ball
             if validDrag (toGrid coords) game then
               let
-                ball_and_history = (Ball (toGrid coords) (game.ball.history ++ [game.ball.coords]))
+                landings = game.ball.landed ++ [game.ball.coords]
+                people_jumped = game.ball.jumped ++ jumpedSquares game.ball.coords (toGrid coords)
+                ball_and_history = Ball (toGrid coords) landings people_jumped
               in
-                (Playing {game | ball = ball_and_history, people = (removeJumped (toGrid coords) game)} {mouse | draggingBall = False, current = coords}, Cmd.none)
+                -- (Playing {game | ball = ball_and_history, people = (removeJumped (toGrid coords) game)} {mouse | draggingBall = False, current = coords}, Cmd.none)
+                (Playing {game | ball = ball_and_history} {mouse | draggingBall = False, current = coords}, Cmd.none)
             else
               (Playing game {mouse | draggingBall = False, current = coords}, Cmd.none)
           else
@@ -190,7 +197,7 @@ view model =
 drawGame : Game -> MouseData -> Html Msg
 drawGame game mouse =
   svg [width (String.fromInt (w + 2 * margin)), height (String.fromInt (h + 2 * margin))]
-  ([background] ++ grid ++ endZones ++ people game.people ++ (drawBall game.ball mouse))
+  ([background] ++ grid ++ endZones ++ people game ++ (drawBall game.ball mouse))
 
 horizontal : Int -> String -> Int -> Svg msg
 horizontal y color width = line
@@ -220,12 +227,13 @@ drawCircle coords radius c = circle
   , fill c
   ] []
 
-stone : Coords -> String -> Svg msg
-stone coords color = circle
+stone : Coords -> String -> Float -> Svg msg
+stone coords color opacity = circle
   [ cx <| String.fromInt (loc <| coords).x
   , cy <| String.fromInt (loc <| coords).y
   , r  <| String.fromFloat (square_width * 0.45)
   , fill color
+  , fillOpacity <| String.fromFloat opacity
   ] []
 
 background : Svg msg
@@ -248,13 +256,13 @@ drawBall ball mouse =
     rad = if mouse.draggingBall then 0.65 * square_width else 0.45 * square_width
     current_circle = drawCircle c rad "white"
     coords_to_str coords = String.fromInt coords.x ++ "," ++ String.fromInt coords.y
-    str_points = String.join " " <| List.map (\x -> coords_to_str (loc x)) (ball.history ++ [ball.coords])
+    str_points = String.join " " <| List.map (\x -> coords_to_str (loc x)) (ball.landed ++ [ball.coords])
     history_path = Svg.polyline [points str_points, stroke "red", strokeWidth "10", fill "none", strokeLinecap "round", strokeLinejoin "round"] []
   in
     [history_path, current_circle]
 
-people : List Coords -> List (Svg msg)
-people coords_list = List.map (\x -> stone x "black") coords_list
+people : Game -> List (Svg msg)
+people game = List.map (\x -> stone x "black" (if List.member x game.ball.jumped then 0.5 else 1.0)) game.people
 
 grid : List (Svg msg)
 grid = (List.map (\x -> vertical x "black" 1) (List.range 0 14)) ++ (List.map (\y -> horizontal y "black" 1) (List.range 1 19))
